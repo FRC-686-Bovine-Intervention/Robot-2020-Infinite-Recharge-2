a2cca55d62f8c8459f4f5c95f660fd2090c7be12
Roame Hildebrand <roamehildebrand@gmail.com>
2020-03-02 16:05:58 -0500

    Returned Robot.java to Intended Form

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import java.util.ResourceBundle.Control;
@@ -27,30 +20,16 @@ import frc.robot.Subsystems.Drivetrain;
 import frc.robot.Subsystems.Intake;
 import frc.robot.Subsystems.Kickers;
 import frc.robot.Subsystems.Lift;
-import frc.robot.Subsystems.Shooter.Flywheel;
-import frc.robot.Subsystems.Shooter.Hood;
-import frc.robot.Subsystems.Shooter.Limelight;
 import frc.robot.Subsystems.Shooter.ShooterMaster;
-import frc.robot.Subsystems.Shooter.Turret;
-import frc.robot.Subsystems.Shooter.Limelight.LedMode;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
   Drivetrain drivetrain;
   Lift lift;
   Intake intake;
   ConveyorBelt conveyorBelt;
   Kickers kickers;
-  //ShooterMaster shooter;
-  Turret turret;
-  Flywheel flywheel;
-  Hood hood;
+  ShooterMaster shooter;
 
   DigitalInput hallEffect;
   
@@ -68,20 +47,14 @@ public class Robot extends TimedRobot {
     intake        = Intake.getInstance();
     conveyorBelt  = ConveyorBelt.getInstance();
     kickers       = Kickers.getInstance();
-    //shooter       = ShooterMaster.getInstance();
-    turret = Turret.getInstance();
-    hood = Hood.getInstance();
-    flywheel = Flywheel.getInstance();
-
+    shooter       = ShooterMaster.getInstance();
     
     drivetrain.start();
     lift.start();       
     intake.start();
     conveyorBelt.start();
     kickers.start();  
-    //shooter.start();
-
-    hallEffect = new DigitalInput(Constants.kTurretHallEffectChannel);
+    shooter.start();
   }
 
   @Override
@@ -90,35 +63,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    startTime = Timer.getFPGATimestamp();
-    kickers.setPercent(Constants.kKickerShootPercent);
-    flywheel.setRPS(275);
-    hood.setPosition(Math.toRadians(22));
   }
 
   @Override
   public void autonomousPeriodic() {
-    double shootingTime = Timer.getFPGATimestamp()-startTime;
-    //wait(2);
-
-    if(shootingTime > 6){
-      kickers.setPercent(0);
-      flywheel.setRPS(0);
-    }
-
-    if(shootingTime > 1 && shootingTime <6){
-      conveyorBelt.turnOnTower();
-      conveyorBelt.turnOnVBelt();
-    } else {
-      conveyorBelt.stopTower();
-      conveyorBelt.stopVBelt();
-    }
-
-    if(shootingTime > 6 && shootingTime <9){
-      drivetrain.setPower(-0.25, -0.25);
-    } else {
-      drivetrain.setPower(0.0, 0.0);
-    }
   }
 
   @Override
@@ -133,67 +81,12 @@ public class Robot extends TimedRobot {
     intake.run();
     conveyorBelt.run();
     kickers.run();  
-    //shooter.run();
-
-    Limelight limelight = Limelight.getInstance();
-
-    if(Controls.getInstance().getBoolean(DriverControlsEnum.SHOOT)){
-      limelight.setLEDMode(LedMode.kOn);
-      if(Controls.getInstance().getBoolean(DriverControlsEnum.MAX_HOOD)){
-        hood.setPosition(Math.toRadians(45));
-      } else {
-        hood.setPosition(Math.toRadians(22));
-      }
-
-      
-
-
-      
-      flywheel.setRPS(275);
-
-    } else {
-      flywheel.setRPS(0);
-      hood.setPosition(0);
-      limelight.setLEDMode(LedMode.kOff);
-    }
-
-    turret.setPosition(0);
-
+    shooter.run();
   }
 
 
-  boolean hoodCalibrated, turretCalibrated, allCalibrated;
-  double hoodLastPos = 0;
-  double hoodCalibTolerance = 0.025;
-  int successfulHoodLoops = 0;
-  int requiredHoodLoops = 3;
-
   @Override
   public void testPeriodic() {
-    if(hallEffect.get() && !turretCalibrated){
-        turret.setPercent(-0.1825);
-    } else {
-        turret.setPercent(0.0);
-        turretCalibrated = true;
-        turret.zeroWithInit(-3);
-    }
-
-    if(Math.abs(hood.getSensedPos()-hoodLastPos) <= hoodCalibTolerance && !hoodCalibrated){
-        successfulHoodLoops++;
-    } else {
-        successfulHoodLoops = 0;
-    }
-    if(!hoodCalibrated && successfulHoodLoops < requiredHoodLoops){
-        hood.setPercent(-0.1825);
-    } else {
-        hood.setPercent(0.0);
-        hood.zeroSensor();
-        hoodCalibrated = true;
-    }
-
-    if(hoodCalibrated && turretCalibrated){
-        allCalibrated = true;
-        return;
-    }
+    
   }
 }
