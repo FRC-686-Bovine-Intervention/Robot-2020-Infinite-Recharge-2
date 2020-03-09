b5d3ffa19f15b4e287e7d085e0f4f79218951802
Roame Hildebrand <roamehildebrand@gmail.com>
2020-03-09 19:54:38 -0400

    Reintroduced Basic Auto

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.ControlStructures.RobotState;
 import frc.robot.ControlStructures.RobotStateLoop;
 import frc.robot.ControlStructures.SubsystemController;
@@ -13,10 +14,13 @@ import frc.robot.Subsystems.Drivetrain;
 import frc.robot.Subsystems.Intake;
 import frc.robot.Subsystems.Kickers;
 import frc.robot.Subsystems.Lift;
+import frc.robot.Subsystems.Shooter.Flywheel;
+import frc.robot.Subsystems.Shooter.Hood;
 import frc.robot.Subsystems.Shooter.Limelight;
 import frc.robot.Subsystems.Shooter.Limelight.LedMode;
 import frc.robot.sensors.Pigeon;
 import frc.robot.util.Pose;
+import frc.robot.util.RisingEdgeDetector;
 import frc.robot.Subsystems.Shooter.ShooterMaster;
 
 
@@ -26,7 +30,9 @@ public class Robot extends TimedRobot {
   CvSink cvSink = CameraServer.getInstance().getVideo();
   CvSource outputStream = CameraServer.getInstance().putVideo("ClimbCam", 640, 480);
   Pigeon pigeon = Pigeon.getInstance();
-  
+
+  private double autoStartTime = 0;
+  private boolean autoStarted = false;  
 
   @Override
   public void disabledInit() {
@@ -61,6 +67,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     subsystemController.resetForCalibration();
+    autoStarted = false;
   }
 
   @Override
@@ -70,7 +77,33 @@ public class Robot extends TimedRobot {
     if(!subsystemController.calibrationComplete()){
       subsystemController.runCalibration();
     } else {
+      if(!autoStarted){
+        autoStartTime = Timer.getFPGATimestamp();
+        autoStarted = true;
+        Kickers.getInstance().setPercent(Constants.kKickerShootPercent);
+        Flywheel.getInstance().setRPS(275);
+        Hood.getInstance().setPosition(Math.toRadians(22));
+      }
 
+      double shootingTime = Timer.getFPGATimestamp()-autoStartTime;
+      if(shootingTime > 6){
+        Kickers.getInstance().setPercent(0);
+        Flywheel.getInstance().setRPS(0);
+      }
+  
+      if(shootingTime > 1 && shootingTime <6){
+        ConveyorBelt.getInstance().turnOnTower();
+        ConveyorBelt.getInstance().turnOnVBelt();
+      } else {
+        ConveyorBelt.getInstance().stopTower();
+        ConveyorBelt.getInstance().stopVBelt();
+      }
+  
+      if(shootingTime > 6 && shootingTime <9){
+        Drivetrain.getInstance().setPower(-0.25, -0.25);
+      } else {
+        Drivetrain.getInstance().setPower(0.0, 0.0);
+      }
     }
   }
 
